16b8e3b1f1235ebbd3d075bb38713b7d317fc228
nasirdiu <nasir.joypurhat@gmail.com>
2022-07-22 15:08:26 +0600

    scrol

--- src/components/Share/Footer.js
+++ src/components/Share/Footer.js
@@ -1,21 +1,39 @@
 import React from "react";
 import { Link } from "react-scroll";
-
+import ScrollToTop from "react-scroll-to-top";
 const Footer = () => {
   return (
-    <div >
+    <div>
       <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
         <div class="grid grid-flow-col gap-4">
-         <a href=""> <Link to="about" spy={true} smooth={true}>About us</Link></a>
-         <a href=""> <Link to="service" spy={true} smooth={true}>Services</Link></a>
-         <a href=""> <Link to="portfolio" spy={true} smooth={true}>Portfolio</Link></a>
-         <a href=""> <Link to="contacts" spy={true} smooth={true}>contacts</Link></a>
-          
+          <a href="">
+            {" "}
+            <Link to="about" spy={true} smooth={true}>
+              About us
+            </Link>
+          </a>
+          <a href="">
+            {" "}
+            <Link to="service" spy={true} smooth={true}>
+              Services
+            </Link>
+          </a>
+          <a href="">
+            {" "}
+            <Link to="portfolio" spy={true} smooth={true}>
+              Portfolio
+            </Link>
+          </a>
+          <a href="">
+            {" "}
+            <Link to="contacts" spy={true} smooth={true}>
+              contacts
+            </Link>
+          </a>
         </div>
         <div>
           <div class="grid grid-flow-col gap-4">
-            <a href="https://twitter.com/nasircse369" target='blank'>
-              
+            <a href="https://twitter.com/nasircse369" target="blank">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -26,7 +44,10 @@ const Footer = () => {
                 <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
               </svg>
             </a>
-            <a href="https://www.youtube.com/channel/UCSi1g3A7nTGWPbUyyabVOWQ" target='blank'>
+            <a
+              href="https://www.youtube.com/channel/UCSi1g3A7nTGWPbUyyabVOWQ"
+              target="blank"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -37,7 +58,7 @@ const Footer = () => {
                 <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
               </svg>
             </a>
-            <a href="https://www.facebook.com/Nasir369/" target='blank'>
+            <a href="https://www.facebook.com/Nasir369/" target="blank">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -51,13 +72,17 @@ const Footer = () => {
           </div>
         </div>
         <div className="text-xl">
-          <p>Copyright © 2022 - <span className="text-orange-500">All right reserved by Nasir 369</span></p>
+          <p>
+            Copyright © 2022 -{" "}
+            <span className="text-orange-500">
+              All right reserved by Nasir 369
+            </span>
+          </p>
         </div>
-        <div className="absolute right-0 mt-40"  id="slider">
-        <a href="#slider" className="text-orange-400 "><button className="btn btn-outline hover:bg-accent btn-sm">Go to Home</button> </a>
+        <div>
+          <ScrollToTop smooth color="#6f00ff" />
         </div>
       </footer>
-      
     </div>
   );
 };
