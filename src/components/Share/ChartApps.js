12cbac7330191a4f684849e999923d29dd0bc357
nasirdiu <nasir.joypurhat@gmail.com>
2022-07-30 11:59:20 +0600

    chat

---
+++ src/components/Share/ChartApps.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat";
+const ChartApps = () => {
+  return (
+    <div>
+      <MessengerCustomerChat
+        pageId="<101895019292258>"
+        appId="<516521953172970>"
+      />
+    </div>
+  );
+};
+
+export default ChartApps;
